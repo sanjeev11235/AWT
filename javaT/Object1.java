62f17e0469eb78223a7c2b032f864d6b97cf4c36
Sanjeev Kumar Saini <sanjeevsaini11235@gmail.com>
2020-02-18 15:37:56 +0530

    p18

---
+++ javaT/Object1.java
@@ -0,0 +1,9 @@
+public class Object1 extends Object {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		System.out.println(Object.class);
+
+	}
+
+}
---
+++ javaT/Test2.java
@@ -0,0 +1,14 @@
+
+class Test1{
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}
+	
+
+}
+
+public class Test2{
+
+	
+}
